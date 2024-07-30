333ced8bb44f617709de552744bbf7dea4148ad9
Cavejon <tehviperry@gmail.com>
2024-07-30 14:20:45 -0300

    New: Lógica e interface de registrar criada

--- ProductivityHub-FE/src/app/app.routes.ts
+++ ProductivityHub-FE/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { LayoutComponent } from './pages/layout/layout.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
   {
@@ -12,6 +13,10 @@ export const routes: Routes = [
     {
       path: 'login',
       component:LoginComponent
+    },
+    {
+      path: 'register',
+      component:RegisterComponent
     },
     {
       path: 'layout',
