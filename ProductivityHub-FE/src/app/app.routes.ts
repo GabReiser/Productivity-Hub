2fc93475b8fba9a9c4c96b3ddb85056dcdb038f9
Cavejon <tehviperry@gmail.com>
2024-07-30 17:55:11 -0300

    Fix: Routes add

--- ProductivityHub-FE/src/app/app.routes.ts
+++ ProductivityHub-FE/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
-import { LayoutComponent } from './pages/layout/layout.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 export const routes: Routes = [
   {
@@ -24,14 +24,12 @@ export const routes: Routes = [
       component:RegisterComponent
     },
     {
-      path: 'layout',
-      component:LayoutComponent,
-      children:[
-        {
-          path:'dashboard',
-          component:DashboardComponent
-        }
-      ]
+      path: 'dashboard',
+      component:DashboardComponent,
+    },
+    {
+      path: 'about-us',
+      component:AboutUsComponent,
     },
 
 ];
