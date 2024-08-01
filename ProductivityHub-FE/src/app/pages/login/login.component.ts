c8ac1d31afdc4603e7e6470384e6bb57f8ab6357
GabReiser <gab.reiser@outlook.com>
2024-07-31 22:04:19 -0300

    ajuste para deploy

--- ProductivityHub-FE/src/app/pages/login/login.component.ts
+++ ProductivityHub-FE/src/app/pages/login/login.component.ts
@@ -3,32 +3,36 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { Router } from '@angular/router';
-
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule , HttpClientModule],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
 
   loginObj: Login;
 
-  constructor(private http: HttpClient, private router: Router){
+  constructor(private http: HttpClient, private router: Router) {
     this.loginObj = new Login();
   }
-  onLogin(){
-    this.http.post('',this.loginObj),subscribe((res:any)=>{
-      if(res.result){
-        alert("login sucess");
-        localStorage.setItem('loginToken', res.data.token)
-        this.router.navigateByUrl('/dashboard')
-      }else{
+
+  onLogin() {
+    this.http.post(`${environment.apiUrl}/login`, this.loginObj).subscribe((res: any) => {
+      if (res.result) {
+        alert("Login successful");
+        localStorage.setItem('loginToken', res.data.token);
+        this.router.navigateByUrl('/dashboard');
+      } else {
         alert(res.message);
       }
-    })
+    }, (error) => {
+      console.error('Error during login:', error);
+      alert('An error occurred during login.');
+    });
   }
 }
 
@@ -36,13 +40,8 @@ export class Login {
   UserName: string;
   Password: string;
 
-  constructor(){
-    this.UserName= '';
-    this.Password= '';
+  constructor() {
+    this.UserName = '';
+    this.Password = '';
   }
-
 }
-function subscribe(arg0: (res: any) => void) {
-  throw new Error('Function not implemented.');
-}
-
---
+++ ProductivityHub-FE/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://productivity-hub.onrender.com/api'  // URL para o ambiente de produção
+};
